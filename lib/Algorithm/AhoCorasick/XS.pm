bc79fb9ffe8b12340b539a6114a11930f3c7bbe0
Richard Harris <rjh@netcraft.com>
2017-03-24 17:36:33 +0000

    Bump version to 0.3

--- lib/Algorithm/AhoCorasick/XS.pm
+++ lib/Algorithm/AhoCorasick/XS.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use List::MoreUtils qw(uniq);
 require XSLoader;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 XSLoader::load('Algorithm::AhoCorasick::XS', $VERSION);
 
 sub unique_matches {
